a7a736479ad89df68a169eeeeb4ced46a33e4396
soumik-kiit <132847046+soumik-kiit@users.noreply.github.com>
2023-05-12 13:50:32 +0530

    Create fileReturn.py

---
+++ fileReturn.py
@@ -0,0 +1,30 @@
+import logging
+import azure.functions as func
+import csv
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    # Sample data
+    data = [
+        ['Name', 'Age', 'Gender'],
+        ['John', '25', 'Male'],
+        ['Jane', '30', 'Female'],
+        ['Bob', '45', 'Male']
+    ]
+
+    # Create CSV file
+    file_path = "example.csv"
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        for row in data:
+            writer.writerow(row)
+
+    # Return CSV file as response
+    with open(file_path, mode='r') as file:
+        file_content = file.read()
+    headers = {
+        'Content-Type': 'text/csv',
+        'Content-Disposition': 'attachment; filename="example.csv"'
+    }
+    return func.HttpResponse(file_content, headers=headers)
